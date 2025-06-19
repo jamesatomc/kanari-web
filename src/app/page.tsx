24a81a89770e5e3353abe087f4e64f52d122793e
jamesatomc <jiraphonwansai2002@gmail.com>
2025-06-19 16:34:35 +0700

    Refactor Home component to simplify structure by removing dark mode props and unnecessary elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,8 @@
-'use client';
 
-import React from 'react';
 import HeroSection from "./HeroSection";
 
-interface HomePageProps {
-  darkMode?: boolean;
-  setDarkMode?: (darkMode: boolean) => void;
-}
-
-export default function Home({ darkMode = false, setDarkMode = () => {} }: HomePageProps) {
+export default function Home() {
   return (
-    <div className={`transition-colors duration-300 ${darkMode
-      ? 'bg-gradient-to-r from-gray-950 to-indigo-950'
-      : 'bg-gradient-to-r from-indigo-100 to-purple-100'
-    }`}>
-      <main className="relative min-h-screen overflow-hidden">
-        {/* Content */}
-        <div className="relative z-10">
-          {/* <Navbar darkMode={darkMode} setDarkMode={setDarkMode} /> */}
-          
-          {/* Pass darkMode props to all child components */}
-          <HeroSection />
-          {/* <LatestUpdates darkMode={darkMode} setDarkMode={setDarkMode} />
-          <InvestorsSection darkMode={darkMode} setDarkMode={setDarkMode} />
-          <OfficialChannels darkMode={darkMode} setDarkMode={setDarkMode} />
-          <NewsletterSection darkMode={darkMode} setDarkMode={setDarkMode} />
-          <Footer darkMode={darkMode} setDarkMode={setDarkMode} /> */}
-        </div>
-      </main>
-    </div>
+    <HeroSection />
   );
 }
